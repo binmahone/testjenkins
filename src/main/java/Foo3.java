43d60db93a7b15e434239264c41f0d7660d53cd9
Hongbin Ma <ma@kyligence.io>
2018-09-06 22:46:46 +0800

    fixj4

---
+++ src/main/java/Foo3.java
@@ -0,0 +1,9 @@
+public class Foo3 {
+    public static int calc(int a, int b) {
+        return 100 * a + b;
+    }
+
+    public static int calc2(int a, int b) {
+        return 99 * a + b;
+    }
+}
---
+++ src/test/java/Foo3Test.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+public class Foo3Test {
+    @Test
+    public void xx() {
+        Foo3.calc(1, 2);
+    }
+
+}
