0e031d4a859a94f961ec45ff81c12921a82fbccb
Hongbin Ma <ma@kyligence.io>
2018-09-06 22:48:28 +0800

    hi

--- src/test/java/Foo3Test.java
+++ src/test/java/Foo3Test.java
@@ -3,6 +3,13 @@ import org.junit.Test;
 public class Foo3Test {
     @Test
     public void xx() {
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
         Foo3.calc(1, 2);
     }
 
