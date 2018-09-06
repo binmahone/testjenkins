62dc4480b7e2ba89d4ca051c48d619576c8e1fa4
Hongbin Ma <ma@kyligence.io>
2018-09-06 22:48:47 +0800

    g

--- src/main/java/Foo3.java
+++ src/main/java/Foo3.java
@@ -4,6 +4,14 @@ public class Foo3 {
     }
 
     public static int calc2(int a, int b) {
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
         return 99 * a + b;
     }
 }
