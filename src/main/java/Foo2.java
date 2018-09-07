c2ec08110552f60ea2bc7b44ece360048d9c216a
Hongbin Ma <ma@kyligence.io>
2018-09-07 09:18:02 +0800

    back

--- src/main/java/Foo2.java
+++
@@ -1,9 +0,0 @@
-public class Foo2 {
-    public static int calc(int a, int b) {
-        return 100 * a + b;
-    }
-
-    public static int calc2(int a, int b) {
-        return 99 * a + b;
-    }
-}
--- src/test/java/Foo2Test.java
+++
@@ -1,9 +0,0 @@
-import org.junit.Test;
-
-public class Foo2Test {
-    @Test
-    public void xx() {
-        Foo2.calc(1, 2);
-    }
-
-}
