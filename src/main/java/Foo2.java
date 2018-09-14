831268f7136d19a291bcc0c352d55535f43e9025
Hongbin Ma <ma@kyligence.io>
2018-09-14 11:39:23 +0800

    new

---
+++ src/main/java/Foo2.java
@@ -0,0 +1,10 @@
+public class Foo2 {
+    public static int calc(int a, int b) {
+        return 100 * a + b;
+    }
+
+    public static int calc2(int a, int b) {
+        System.out.println();
+        return 99 * a + b;
+    }
+}
